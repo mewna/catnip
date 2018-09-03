9dbc51fdd3597f1d39a470ca5995bb5c12ca189f
amy null <null@amy.gg>
2018-09-02 21:19:28 -0400

    Edit messages endpoint, clean up some REST handling code

--- src/main/java/com/mewna/catnip/rest/Rest.java
+++ src/main/java/com/mewna/catnip/rest/Rest.java
@@ -18,6 +18,7 @@ import javax.annotation.Nonnull;
  * @author amy
  * @since 9/1/18.
  */
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class Rest {
     @Getter
     private final Catnip catnip;
@@ -26,6 +27,7 @@ public class Rest {
         this.catnip = catnip;
     }
     
+    @Nonnull
     public Future<Message> createMessage(@Nonnull final String channelId, @Nonnull final String content) {
         return createMessage(channelId, new MessageBuilder().content(content).build());
     }
@@ -50,8 +52,31 @@ public class Rest {
     @Nonnull
     @CheckReturnValue
     public Future<Message> getMessage(@Nonnull final String channelId, @Nonnull final String messageId) {
-        return catnip._requester().queue(new OutboundRequest(Routes.GET_CHANNEL_MESSAGE.withMajorParam(channelId).compile("message.id", messageId),
-                ImmutableMap.of(), null
-        )).map(EntityBuilder::createMessage);
+        return catnip._requester().queue(
+                new OutboundRequest(Routes.GET_CHANNEL_MESSAGE.withMajorParam(channelId),
+                        ImmutableMap.of("message.id", messageId), null)).map(EntityBuilder::createMessage);
+    }
+    
+    @Nonnull
+    public Future<Message> editMessage(@Nonnull final String channelId, @Nonnull final String messageId,
+                                       @Nonnull final String content) {
+        return editMessage(channelId, messageId, new MessageBuilder().content(content).build());
+    }
+    
+    @Nonnull
+    public Future<Message> editMessage(@Nonnull final String channelId, @Nonnull final String messageId,
+                                       @Nonnull final Message message) {
+        final JsonObject json = new JsonObject();
+        if(message.content() != null && !message.content().isEmpty()) {
+            json.put("content", message.content());
+        }
+        if(message.embeds() != null && !message.embeds().isEmpty()) {
+            json.put("embeds", new JsonArray(message.embeds()));
+        }
+        if(json.getValue("embeds", null) == null && json.getValue("content", null) == null) {
+            throw new IllegalArgumentException("Can't build a message with no content and no embeds!");
+        }
+        return catnip._requester().queue(new OutboundRequest(Routes.EDIT_MESSAGE.withMajorParam(channelId),
+                ImmutableMap.of("message.id", messageId), json)).map(EntityBuilder::createMessage);
     }
 }
