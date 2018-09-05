8feab0e68517683b9f5792eb3d699cbd97b18fe6
Max Grosshandler <maxgrosshandler@gmail.com>
2018-09-05 18:20:43 -0500

    adjusted formatting and added in some missing code to help satisfy the pr

--- src/main/java/com/mewna/catnip/entity/builder/EmbedBuilder.java
+++ src/main/java/com/mewna/catnip/entity/builder/EmbedBuilder.java
@@ -17,7 +17,6 @@ import java.util.List;
  */
 @SuppressWarnings({"WeakerAccess", "unused"})
 public class EmbedBuilder {
-    
     // @formatter:off
     private String title;
     private String description;
@@ -140,10 +139,186 @@ public class EmbedBuilder {
     }
     
     public Embed build() {
-        int len = 0;
         final EmbedImplBuilder builder = EmbedImpl.builder();
         if(title != null && !title.isEmpty()) {
-            if (title.length() > 256){
+            builder.title(title);
+        }
+        if(description != null && !description.isEmpty()) {
+            builder.description(description);
+        }
+        if(url != null && !url.isEmpty()) {
+            builder.url(url);
+        }
+        if(color != null) {
+            builder.color(color);
+        }
+        if(footer != null) {
+            builder.footer(footer);
+        }
+        if(image != null) {
+            builder.image(image);
+        }
+        if(thumbnail != null) {
+            builder.thumbnail(thumbnail);
+        }
+        if(author != null) {
+            builder.author(author);
+        }
+        if(fields.isEmpty()) {
+            builder.fields(Collections.emptyList());
+        } else {
+            if(fields.size() > 25) {
+                throw new IllegalStateException("Tried to add an embed field, but we're at the cap (25)!");
+            }
+            builder.fields(fields);
+        }
+        return builder.build();
+    }
+}
+package com.mewna.catnip.entity.builder;
+
+import com.mewna.catnip.entity.impl.RichEmbed;
+import com.mewna.catnip.entity.impl.RichEmbed.*;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author amy
+ * @since 9/4/18.
+ */
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class EmbedBuilder {
+    
+    // @formatter:off
+    private String title;
+    private String description;
+    private String url;
+    private Integer color;
+    private Footer footer;
+    private Image image;
+    private Thumbnail thumbnail;
+    private Author author;
+    private final List<Field> fields = new ArrayList<>();
+    // @formatter:on
+    
+    public EmbedBuilder() {
+    }
+    
+    public EmbedBuilder(final RichEmbed embed) {
+        throw new UnsupportedOperationException("Building embeds from existing embeds is currently unsupported");
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder title(@Nullable final String title) {
+        this.title = title;
+        return this;
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder description(@Nullable final String description) {
+        this.description = description;
+        return this;
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder url(@Nullable final String url) {
+        this.url = url;
+        return this;
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder color(@Nullable final Integer color) {
+        this.color = color;
+        return this;
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder footer(@Nullable final String text) {
+        return footer(text, null);
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder footer(@Nullable final String text, @Nullable final String iconUrl) {
+        return footer(new Footer(text, iconUrl, null));
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder footer(@Nullable final Footer footer) {
+        this.footer = footer;
+        return this;
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder image(@Nullable final String url) {
+        image = new Image(url, null, 0, 0);
+        return this;
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder thumbnail(@Nullable final String url) {
+        thumbnail = new Thumbnail(url, null, 0, 0);
+        return this;
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder author(@Nullable final String name) {
+        return author(name, null);
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder author(@Nullable final String name, @Nullable final String url) {
+        return author(name, url, null);
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder author(@Nullable final String name, @Nullable final String url, @Nullable final String iconUrl) {
+        return author(new Author(name, url, iconUrl, null));
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder author(@Nullable final Author author) {
+        this.author = author;
+        return this;
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder field(@Nonnull final String name, @Nonnull final String value, final boolean inline) {
+        return field(new Field(name, value, inline));
+    }
+    
+    @Nonnull
+    @CheckReturnValue
+    public EmbedBuilder field(@Nonnull final Field field) {
+        if(fields.size() == 25) {
+            throw new IllegalStateException("Tried to add an embed field, but we're at the cap (25)!");
+        }
+        fields.add(field);
+        return this;
+    }
+    
+    public RichEmbed build() {
+        int len = 0;
+        final RichEmbedBuilder builder = RichEmbed.builder();
+        if(title != null && !title.isEmpty()) {
+            if(title.length() > 256){
                 throw new IllegalStateException("Title exceeds 256 characters!");
             }
             len += title.length();
@@ -151,7 +326,7 @@ public class EmbedBuilder {
             
         }
         if(description != null && !description.isEmpty()) {
-            if (description.length() > 2048){
+            if(description.length() > 2048){
                 throw new IllegalStateException("Description exceeds 2048 characters!");
             }
             builder.description(description);
@@ -164,7 +339,7 @@ public class EmbedBuilder {
             builder.color(color);
         }
         if(footer != null) {
-            if (footer.text().length() > 2048){
+            if(footer.text().length() > 2048){
                 throw new IllegalStateException("Footer text exceeds 2048 characters!");
             }
             builder.footer(footer);
@@ -177,30 +352,32 @@ public class EmbedBuilder {
             builder.thumbnail(thumbnail);
         }
         if(author != null) {
-            if (author.name().length() > 256){
+            if(author.name().length() > 256){
                 throw new IllegalStateException("Author's name exceeds 256 characters!");
             }
             len += author.name().length();
             builder.author(author);
         }
-        if(!fields.isEmpty()) {
+        if(fields.isEmpty()) {
+        	builder.fields(Collections.emptyList());
+        } else {
             if(fields.size() > 25) {
                 throw new IllegalStateException("Tried to add an embed field, but we're at the cap (25)!");
             }
-            for (Field item : fields) {
-                if (item.name().length() > 256){
-                throw new IllegalStateException("Field name exceeds 256 characters!");
+            for(Field field : fields) {
+                if(field.name().length() > 256){
+                    throw new IllegalStateException("Field name exceeds 256 characters!");
+                }
+                if(field.value().length() > 1024){
+                    throw new IllegalStateException("Field value exceeds 1024 characters!");
+                }
+                len += field.name().length();
+                len += field.value().length();
             }
-            if (item.value().length() > 1024){
-                throw new IllegalStateException("Field value exceeds 1024 characters!");
-            }
-                len += item.name().length();
-                len += item.value().length();
-}
             builder.fields(fields);
         }
-         if (len > 6000){
-                throw new IllegalStateException("Total embed length exceeds 6000 characters!");
+         if(len > 6000){
+             throw new IllegalStateException("Total embed length exceeds 6000 characters!");
             }
         return builder.build();
     }
