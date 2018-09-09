585e77678a2225c4188525c9e970dbb8945e4694
amy null <null@amy.gg>
2018-09-09 15:47:46 -0400

    Oh also remove an unneded import oops

--- src/main/java/com/mewna/catnip/shard/event/CachingBuffer.java
+++ src/main/java/com/mewna/catnip/shard/event/CachingBuffer.java
@@ -5,10 +5,11 @@ import io.vertx.core.json.JsonObject;
 import lombok.Value;
 import lombok.experimental.Accessors;
 
-import java.util.*;
+import java.util.Deque;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 /**
