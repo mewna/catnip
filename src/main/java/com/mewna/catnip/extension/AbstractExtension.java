a02d7aa662f315421a0c354e883040c5c092a146
amy null <null@amy.gg>
2018-09-09 20:03:41 -0400

    Fix a missing method on AbstractExtension

--- src/main/java/com/mewna/catnip/extension/AbstractExtension.java
+++ src/main/java/com/mewna/catnip/extension/AbstractExtension.java
@@ -1,8 +1,10 @@
 package com.mewna.catnip.extension;
 
+import com.mewna.catnip.Catnip;
 import io.vertx.core.AbstractVerticle;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 /**
@@ -13,6 +15,10 @@ import lombok.experimental.Accessors;
 @SuppressWarnings("WeakerAccess")
 @RequiredArgsConstructor
 public abstract class AbstractExtension extends AbstractVerticle implements Extension {
+    @Getter
+    @Setter
+    private Catnip catnip;
+    
     @Getter
     private final String name;
 }
