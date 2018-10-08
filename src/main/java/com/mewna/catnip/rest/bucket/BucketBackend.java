8b4763dd94411caf3aa815b4459af0dec412b126
amy null <null@amy.gg>
2018-10-07 22:20:39 -0400

    Fix broken ratelimit logic

--- src/main/java/com/mewna/catnip/rest/bucket/BucketBackend.java
+++ src/main/java/com/mewna/catnip/rest/bucket/BucketBackend.java
@@ -1,16 +1,39 @@
 package com.mewna.catnip.rest.bucket;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
 /**
  * @author amy
  * @since 10/7/18.
  */
+@SuppressWarnings("UnusedReturnValue")
 public interface BucketBackend {
     BucketBackend limit(String route, long value);
+    
     long limit(String route);
     
     BucketBackend remaining(String route, long value);
+    
     long remaining(String route);
     
     BucketBackend reset(String route, long value);
+    
     long reset(String route);
+    
+    @Getter
+    @Setter
+    @Accessors(fluent = true)
+    @SuppressWarnings("FieldMayBeFinal")
+    @NoArgsConstructor
+    final class Container {
+        // By default, we pretend we have 1 request left in a 5-limit bucket.
+        // This is done so that it'll immediately update from the headers on
+        // the next request
+        private long limit = 5;
+        private long remaining = 1;
+        private long reset = System.currentTimeMillis() - 1L;
+    }
 }
