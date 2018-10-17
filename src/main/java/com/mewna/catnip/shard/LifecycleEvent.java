b59fc17d706d219cb7812df24d4d0152429360c1
amy null <null@amy.gg>
2018-10-17 11:06:26 -0400

    Doc shard lifecycle events

--- src/main/java/com/mewna/catnip/shard/LifecycleEvent.java
+++ src/main/java/com/mewna/catnip/shard/LifecycleEvent.java
@@ -8,10 +8,32 @@ import static com.mewna.catnip.shard.EventTypeImpl.event;
  */
 public interface LifecycleEvent {
     // @formatter:off
+    /**
+     * Fired when the shard is created and is about to connect to the websocket
+     * gateway. The payload is a shard id / total pair.
+     */
     EventType<ShardInfo> CONNECTING   = event(Raw.CONNECTING, ShardInfo.class);
+    /**
+     * Fired when the shard has connected to the websocket gateway, but has not
+     * yet sent an IDENTIFY payload. The payload is a shard id / total pair.
+     */
     EventType<ShardInfo> CONNECTED    = event(Raw.CONNECTED, ShardInfo.class);
+    /**
+     * Fired when the shard has disconnected from the websocket gateway, and
+     * will (hopefully) be reconnecting. The payload is a shard id / total
+     * pair.
+     */
     EventType<ShardInfo> DISCONNECTED = event(Raw.DISCONNECTED, ShardInfo.class);
+    /**
+     * Fired when the shard has successfully IDENTIFYd with the websocket
+     * gateway. This is effectively the same as listening on
+     * {@link DiscordEvent#READY}. The payload is a shard id / total pair.
+     */
     EventType<ShardInfo> IDENTIFIED   = event(Raw.IDENTIFIED, ShardInfo.class);
+    /**
+     * Fired when the shard has successfully RESUMEd with the websocket
+     * gateway. The payload is a shard id / total pair.
+     */
     EventType<ShardInfo> RESUMED      = event(Raw.RESUMED, ShardInfo.class);
     // @formatter:on
     
