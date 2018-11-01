6788c1e2d317a025b63e8c21d0b575e4bc6f87f2
amy null <null@amy.gg>
2018-10-31 23:06:02 -0400

    Even more JSON deserialization memes

--- src/main/java/com/mewna/catnip/entity/channel/UserDMChannel.java
+++ src/main/java/com/mewna/catnip/entity/channel/UserDMChannel.java
@@ -1,6 +1,8 @@
 package com.mewna.catnip.entity.channel;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.mewna.catnip.entity.impl.UserDMChannelImpl;
 import com.mewna.catnip.entity.user.User;
 
 import javax.annotation.CheckReturnValue;
@@ -10,6 +12,7 @@ import javax.annotation.Nullable;
  * @author natanbc
  * @since 9/12/18
  */
+@JsonDeserialize(as = UserDMChannelImpl.class)
 public interface UserDMChannel extends DMChannel {
     @Nullable
     @CheckReturnValue
