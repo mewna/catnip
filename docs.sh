1ad2c52279834ea9dd951397a6d6e815d9c5c62c
amy null <null@amy.gg>
2019-01-16 19:33:11 -0500

    Update docs because I forgot to pull master

--- docs.sh
+++ docs.sh
@@ -3,6 +3,7 @@
 # Build docs w/ Java 8
 # This exists mainly for me because javadoc tooling is dumb with lombok
 
+rm -rf docs
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk/"
 mvn clean javadoc:javadoc
 cp -r target/site/apidocs ./docs
