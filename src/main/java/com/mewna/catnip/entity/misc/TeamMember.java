d81414ad57e81d3f51c614f35a212c6cfc7498c5
Bowser65 <contact.bowser65@gmail.com>
2019-06-24 19:37:46 +0200

    Implement teams

---
+++ src/main/java/com/mewna/catnip/entity/misc/TeamMember.java
@@ -0,0 +1,50 @@
+package com.mewna.catnip.entity.misc;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.mewna.catnip.entity.impl.TeamMemberImpl;
+import com.mewna.catnip.entity.user.User;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * Information about a team member.
+ *
+ * @author Bowser65
+ * @since 06/24/19.
+ */
+@JsonDeserialize(as = TeamMemberImpl.class)
+public interface TeamMember {
+    /**
+     * @return The membership state. Either 1 for pending, 2 for accepted.
+     */
+    int membershipState();
+    
+    /**
+     * @return The permissions of the member. Will always be ["*"] as teams doesn't have permissions yet.
+     */
+    @Nonnull
+    List<String> permissions();
+    
+    /**
+     * @return The ID of the parent team they are member of.
+     */
+    @CheckReturnValue
+    default String teamId() {
+        return Long.toUnsignedString(teamIdAsLong());
+    }
+    
+    /**
+     * @return The ID of the parent team they are member of, as a long.
+     */
+    @CheckReturnValue
+    long teamIdAsLong();
+    
+    /**
+     * @return The user.
+     */
+    @Nonnull
+    @CheckReturnValue
+    User user();
+}
