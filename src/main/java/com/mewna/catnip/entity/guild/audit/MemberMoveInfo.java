15b0be004dc2170715583ef2939e77e73f112e31
KJP12 <kjp12.k.joy.programer12@gmail.com>
2020-03-19 00:20:06 -0700

    Added new audit action types and allowed (un)pinning to have a reason

--- src/main/java/com/mewna/catnip/entity/guild/audit/MemberMoveInfo.java
+++ src/main/java/com/mewna/catnip/entity/guild/audit/MemberMoveInfo.java
@@ -27,68 +27,23 @@
 
 package com.mewna.catnip.entity.guild.audit;
 
-import lombok.Getter;
-
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 
 /**
- * @author SamOphis
- * @since 10/07/18
+ * @author kjp12
+ * @since March 18th, 2020
  */
-@SuppressWarnings("unused")
-public enum ActionType {
-    GUILD_UPDATE(1),
-    CHANNEL_CREATE(10),
-    CHANNEL_UPDATE(11),
-    CHANNEL_DELETE(12),
-    CHANNEL_OVERWRITE_CREATE(13),
-    CHANNEL_OVERWRITE_UPDATE(14),
-    CHANNEL_OVERWRITE_DELETE(15),
-    MEMBER_KICK(20),
-    MEMBER_PRUNE(21),
-    MEMBER_BAN_ADD(22),
-    MEMBER_BAN_REMOVE(23),
-    MEMBER_UPDATE(24),
-    MEMBER_ROLE_UPDATE(25),
-    MEMBER_MOVE(26),
-    MEMBER_DISCONNECT(27),
-    BOT_ADD(28),
-    ROLE_CREATE(30),
-    ROLE_UPDATE(31),
-    ROLE_DELETE(32),
-    INVITE_CREATE(40),
-    INVITE_UPDATE(41),
-    INVITE_DELETE(42),
-    WEBHOOK_CREATE(50),
-    WEBHOOK_UPDATE(51),
-    WEBHOOK_DELETE(52),
-    EMOJI_CREATE(60),
-    EMOJI_UPDATE(61),
-    EMOJI_DELETE(62),
-    MESSAGE_DELETE(72),
-    MESSAGE_BULK_DELETE(73),
-    MESSAGE_PIN(74),
-    MESSAGE_UNPIN(75),
-    INTEGRATION_CREATE(80),
-    INTEGRATION_UPDATE(81),
-    INTEGRATION_DELETE(82);
-    
-    @Getter
-    private final int value;
-    
-    ActionType(final int value) {
-        this.value = value;
-    }
-    
+public interface MemberMoveInfo extends OptionalEntryInfo {
     @Nonnull
     @CheckReturnValue
-    public static ActionType byKey(final int value) {
-        for(final ActionType type : values()) {
-            if(type.value == value) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("No action type for value: " + value);
+    default String channelId() {
+        return Long.toUnsignedString(channelIdAsLong());
     }
+    
+    @CheckReturnValue
+    long channelIdAsLong();
+    
+    @CheckReturnValue
+    int membersMovedCount();
 }
--- src/main/java/com/mewna/catnip/entity/impl/guild/audit/MessageBulkDeleteInfoImpl.java
+++ src/main/java/com/mewna/catnip/entity/impl/guild/audit/MessageBulkDeleteInfoImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 amy, All rights reserved.
+ * Copyright (c) 2019 amy, All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -25,70 +25,37 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package com.mewna.catnip.entity.guild.audit;
+package com.mewna.catnip.entity.impl.guild.audit;
 
-import lombok.Getter;
+import com.mewna.catnip.Catnip;
+import com.mewna.catnip.entity.RequiresCatnip;
+import com.mewna.catnip.entity.guild.audit.MessageBulkDeleteInfo;
+import lombok.*;
+import lombok.experimental.Accessors;
 
-import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 
 /**
- * @author SamOphis
- * @since 10/07/18
+ * The data returned for MESSAGE_BULK_DELETE <i>doesn't</i> actually follow the API documentation.
+ * If you're looking for the channel ID that's normally returned from here like in {@link com.mewna.catnip.entity.guild.audit.MessageDeleteInfo MessageDeleteInfo},
+ * use {@link com.mewna.catnip.entity.guild.audit.AuditLogEntry#targetId() AuditLogEntry#targetId()} or {@link com.mewna.catnip.entity.guild.audit.AuditLogEntry#targetIdAsLong() AuditLogEntry#targetIdAsLong()}.
+ *
+ * @author kjp12
+ * @since March 18th, 2020
  */
-@SuppressWarnings("unused")
-public enum ActionType {
-    GUILD_UPDATE(1),
-    CHANNEL_CREATE(10),
-    CHANNEL_UPDATE(11),
-    CHANNEL_DELETE(12),
-    CHANNEL_OVERWRITE_CREATE(13),
-    CHANNEL_OVERWRITE_UPDATE(14),
-    CHANNEL_OVERWRITE_DELETE(15),
-    MEMBER_KICK(20),
-    MEMBER_PRUNE(21),
-    MEMBER_BAN_ADD(22),
-    MEMBER_BAN_REMOVE(23),
-    MEMBER_UPDATE(24),
-    MEMBER_ROLE_UPDATE(25),
-    MEMBER_MOVE(26),
-    MEMBER_DISCONNECT(27),
-    BOT_ADD(28),
-    ROLE_CREATE(30),
-    ROLE_UPDATE(31),
-    ROLE_DELETE(32),
-    INVITE_CREATE(40),
-    INVITE_UPDATE(41),
-    INVITE_DELETE(42),
-    WEBHOOK_CREATE(50),
-    WEBHOOK_UPDATE(51),
-    WEBHOOK_DELETE(52),
-    EMOJI_CREATE(60),
-    EMOJI_UPDATE(61),
-    EMOJI_DELETE(62),
-    MESSAGE_DELETE(72),
-    MESSAGE_BULK_DELETE(73),
-    MESSAGE_PIN(74),
-    MESSAGE_UNPIN(75),
-    INTEGRATION_CREATE(80),
-    INTEGRATION_UPDATE(81),
-    INTEGRATION_DELETE(82);
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(fluent = true)
+public class MessageBulkDeleteInfoImpl implements MessageBulkDeleteInfo, RequiresCatnip {
+    private transient Catnip catnip;
     
-    @Getter
-    private final int value;
+    private int deletedMessagesCount;
     
-    ActionType(final int value) {
-        this.value = value;
-    }
-    
-    @Nonnull
-    @CheckReturnValue
-    public static ActionType byKey(final int value) {
-        for(final ActionType type : values()) {
-            if(type.value == value) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("No action type for value: " + value);
+    @Override
+    public void catnip(@Nonnull final Catnip catnip) {
+        this.catnip = catnip;
     }
 }
