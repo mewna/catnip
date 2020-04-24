3c53522d716cceab3b1aeac3f77f6b19a1b223a2
amy null <null@amy.gg>
2020-04-24 17:47:23 -0400

    Docs update

--- docs.sh
+++ docs.sh
@@ -4,6 +4,6 @@
 # This exists mainly for me because javadoc tooling is dumb with lombok
 
 rm -rf docs
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk/"
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk/"
 mvn clean compile javadoc:javadoc
 cp -r target/site/apidocs ./docs
