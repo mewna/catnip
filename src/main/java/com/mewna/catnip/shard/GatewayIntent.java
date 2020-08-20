77c64e7f959d47e9596f3a526e12e192b0439396
amy null <null@amy.gg>
2020-08-20 09:57:51 -0400

    Some intents cleanup

--- src/main/java/com/mewna/catnip/shard/GatewayIntent.java
+++ src/main/java/com/mewna/catnip/shard/GatewayIntent.java
@@ -27,11 +27,13 @@
 
 package com.mewna.catnip.shard;
 
+import com.mewna.catnip.shard.DiscordEvent.Raw;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
 import javax.annotation.Nonnull;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -51,6 +53,7 @@ public enum GatewayIntent {
      * Encapsulates the following events:
      * <ul>
      *     <li>{@link DiscordEvent#GUILD_CREATE}</li>
+     *     <li>{@link DiscordEvent#GUILD_UPDATE}</li>
      *     <li>{@link DiscordEvent#GUILD_DELETE}</li>
      *     <li>{@link DiscordEvent#GUILD_ROLE_CREATE}</li>
      *     <li>{@link DiscordEvent#GUILD_ROLE_UPDATE}</li>
@@ -62,7 +65,17 @@ public enum GatewayIntent {
      * </ul>
      */
     @SuppressWarnings("PointlessBitwiseExpression")
-    GUILDS(1 << 0, false),
+    GUILDS(1 << 0, false, List.of(
+            Raw.GUILD_CREATE,
+            Raw.GUILD_DELETE,
+            Raw.GUILD_ROLE_CREATE,
+            Raw.GUILD_ROLE_UPDATE,
+            Raw.GUILD_ROLE_DELETE,
+            Raw.CHANNEL_CREATE,
+            Raw.CHANNEL_UPDATE,
+            Raw.CHANNEL_DELETE,
+            Raw.CHANNEL_PINS_UPDATE
+    )),
     
     /**
      * <ul>
@@ -71,7 +84,11 @@ public enum GatewayIntent {
      *     <li>{@link DiscordEvent#GUILD_MEMBER_REMOVE}</li>
      * </ul>
      */
-    GUILD_MEMBERS(1 << 1, true),
+    GUILD_MEMBERS(1 << 1, true, List.of(
+            Raw.GUILD_MEMBER_ADD,
+            Raw.GUILD_MEMBER_UPDATE,
+            Raw.GUILD_MEMBER_REMOVE
+    )),
     
     /**
      * <ul>
@@ -79,28 +96,37 @@ public enum GatewayIntent {
      *     <li>{@link DiscordEvent#GUILD_BAN_REMOVE}</li>
      * </ul>
      */
-    GUILD_BANS(1 << 2, false),
+    GUILD_BANS(1 << 2, false, List.of(
+            Raw.GUILD_BAN_ADD,
+            Raw.GUILD_BAN_REMOVE
+    )),
     
     /**
      * <ul>
      *     <li>{@link DiscordEvent#GUILD_EMOJIS_UPDATE}</li>
      * </ul>
      */
-    GUILD_EMOJIS(1 << 3, false),
+    GUILD_EMOJIS(1 << 3, false, List.of(
+            Raw.GUILD_EMOJIS_UPDATE
+    )),
     
     /**
      * <ul>
      *     <li>{@link DiscordEvent#GUILD_INTEGRATIONS_UPDATE}</li>
      * </ul>
      */
-    GUILD_INTEGRATIONS(1 << 4, false),
+    GUILD_INTEGRATIONS(1 << 4, false, List.of(
+            Raw.GUILD_INTEGRATIONS_UPDATE
+    )),
     
     /**
      * <ul>
      *     <li>{@link DiscordEvent#WEBHOOKS_UPDATE}</li>
      * </ul>
      */
-    GUILD_WEBHOOKS(1 << 5, false),
+    GUILD_WEBHOOKS(1 << 5, false, List.of(
+            Raw.WEBHOOKS_UPDATE
+    )),
     
     /**
      * <ul>
@@ -109,14 +135,19 @@ public enum GatewayIntent {
      * </ul>
      * Note that these events are currently undocumented!
      */
-    GUILD_INVITES(1 << 6, false),
+    GUILD_INVITES(1 << 6, false, List.of(
+            Raw.INVITE_CREATE,
+            Raw.INVITE_DELETE
+    )),
     
     /**
      * <ul>
      *     <li>{@link DiscordEvent#VOICE_STATE_UPDATE}</li>
      * </ul>
      */
-    GUILD_VOICE_STATES(1 << 7, false),
+    GUILD_VOICE_STATES(1 << 7, false, List.of(
+            Raw.VOICE_STATE_UPDATE
+    )),
     
     /**
      * <ul>
@@ -125,28 +156,39 @@ public enum GatewayIntent {
      * Note that bots cannot receive presence updates in DMs.<br />
      * <strong>This is a privileged intent.</strong>
      */
-    GUILD_PRESENCES(1 << 8, true),
+    GUILD_PRESENCES(1 << 8, true, List.of(
+            Raw.PRESENCE_UPDATE
+    )),
     
     /**
      * <ul>
      *     <li>{@link DiscordEvent#MESSAGE_CREATE}</li>
      *     <li>{@link DiscordEvent#MESSAGE_UPDATE}</li>
      *     <li>{@link DiscordEvent#MESSAGE_DELETE}</li>
+     *     <li>{@link DiscordEvent#MESSAGE_DELETE_BULK}</li>
      * </ul>
      */
-    GUILD_MESSAGES(1 << 9, false),
+    GUILD_MESSAGES(1 << 9, false, List.of(
+            Raw.MESSAGE_CREATE,
+            Raw.MESSAGE_UPDATE,
+            Raw.MESSAGE_DELETE,
+            Raw.MESSAGE_DELETE_BULK
+    )),
     
     /**
      * <ul>
      *     <li>{@link DiscordEvent#MESSAGE_REACTION_ADD}</li>
      *     <li>{@link DiscordEvent#MESSAGE_REACTION_REMOVE}</li>
      *     <li>{@link DiscordEvent#MESSAGE_REACTION_REMOVE_ALL}</li>
-     *     <li><code>MESSAGE_REACTION_REMOVE_EMOJI</code></li>
+     *     <li>{@link DiscordEvent#MESSAGE_REACTION_REMOVE_EMOJI}</li>
      * </ul>
-     * Note that <code>MESSAGE_REACTION_REMOVE_EMOJI</code> is currently
-     * undocumented pending release of the corresponding REST endpoint.
      */
-    GUILD_MESSAGE_REACTIONS(1 << 10, false),
+    GUILD_MESSAGE_REACTIONS(1 << 10, false, List.of(
+            Raw.MESSAGE_REACTION_ADD,
+            Raw.MESSAGE_REACTION_REMOVE,
+            Raw.MESSAGE_REACTION_REMOVE_ALL,
+            Raw.MESSAGE_REACTION_REMOVE_EMOJI
+    )),
     
     /**
      * <ul>
@@ -155,7 +197,9 @@ public enum GatewayIntent {
      * Note that this toggles <code>TYPING_START</code> events <strong>in
      * guilds only.</strong>
      */
-    GUILD_MESSAGE_TYPING(1 << 11, false),
+    GUILD_MESSAGE_TYPING(1 << 11, false, List.of(
+            Raw.TYPING_START
+    )),
     
     /**
      * <ul>
@@ -163,21 +207,31 @@ public enum GatewayIntent {
      *     <li>{@link DiscordEvent#MESSAGE_CREATE}</li>
      *     <li>{@link DiscordEvent#MESSAGE_UPDATE}</li>
      *     <li>{@link DiscordEvent#MESSAGE_DELETE}</li>
+     *     <li>{@link DiscordEvent#CHANNEL_PINS_UPDATE}</li>
      * </ul>
      */
-    DIRECT_MESSAGES(1 << 12, false),
+    DIRECT_MESSAGES(1 << 12, false, List.of(
+            Raw.CHANNEL_CREATE,
+            Raw.MESSAGE_CREATE,
+            Raw.MESSAGE_UPDATE,
+            Raw.MESSAGE_DELETE,
+            Raw.CHANNEL_PINS_UPDATE
+    )),
     
     /**
      * <ul>
      *     <li>{@link DiscordEvent#MESSAGE_REACTION_ADD}</li>
      *     <li>{@link DiscordEvent#MESSAGE_REACTION_REMOVE}</li>
      *     <li>{@link DiscordEvent#MESSAGE_REACTION_REMOVE_ALL}</li>
-     *     <li><code>MESSAGE_REACTION_REMOVE_EMOJI</code></li>
+     *     <li>{@link DiscordEvent#MESSAGE_REACTION_REMOVE_EMOJI}</li>
      * </ul>
-     * Note that <code>MESSAGE_REACTION_REMOVE_EMOJI</code> is currently
-     * undocumented pending release of the corresponding REST endpoint.
      */
-    DIRECT_MESSAGE_REACTIONS(1 << 13, false),
+    DIRECT_MESSAGE_REACTIONS(1 << 13, false, List.of(
+            Raw.MESSAGE_REACTION_ADD,
+            Raw.MESSAGE_REACTION_REMOVE,
+            Raw.MESSAGE_REACTION_REMOVE_ALL,
+            Raw.MESSAGE_REACTION_REMOVE_EMOJI
+    )),
     
     /**
      * <ul>
@@ -186,7 +240,10 @@ public enum GatewayIntent {
      * Note that this toggles <code>TYPING_START</code> events <strong>in
      * direct messages only.</strong>
      */
-    DIRECT_MESSAGE_TYPING(1 << 14, false),
+    DIRECT_MESSAGE_TYPING(1 << 14, false, List.of(
+            Raw.TYPING_START
+    )),
+    
     ;
     public static final Set<GatewayIntent> ALL_INTENTS = Set.of(values());
     public static final Set<GatewayIntent> UNPRIVILEGED_INTENTS = Set.of(Arrays.stream(values())
@@ -196,10 +253,13 @@ public enum GatewayIntent {
     private final int value;
     @Getter
     private final boolean privileged;
+    @Getter
+    private final List<String> events;
     
-    GatewayIntent(final int value, final boolean privileged) {
+    GatewayIntent(final int value, final boolean privileged, final List<String> events) {
         this.value = value;
         this.privileged = privileged;
+        this.events = events;
     }
     
     public static long from(@Nonnull final Iterable<GatewayIntent> intents) {
