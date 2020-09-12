1b678ce426e06382c5491a9bef90ad52939373cd
amy null <null@amy.gg>
2020-09-11 20:06:43 -0400

    I hate javadocs ;-;

--- docs.sh
+++ docs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf docs
-mvn clean compile javadoc:javadoc
+env JAVA_HOME="/usr/lib/jvm/java-14-openjdk" mvn clean compile javadoc:javadoc
 cp -r target/site/apidocs ./docs
