2544b110bfb13cc4aff81515441c274aa457cb8f
amy null <null@amy.gg>
2021-05-11 01:30:13 -0400

    Fix tests

--- src/test/java/com/mewna/catnip/permission/BitwiseTest.java
+++ src/test/java/com/mewna/catnip/permission/BitwiseTest.java
@@ -44,7 +44,7 @@ public class BitwiseTest {
     @Test
     public void testAll() {
         final Permission[] perms = values();
-        final long expected = 4294967295L;
+        final long expected = 128849018879L;
         long total = 0;
         
         for(final Permission p : perms) {
